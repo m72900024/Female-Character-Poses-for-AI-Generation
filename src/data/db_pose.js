5a5fbdbeb1556d1c578595d16c36145d03bc0244
m72900024 <m72900024@gmail.com>
2026-01-19 21:17:56 +0800

    Create db_pose.js

---
+++ src/data/db_pose.js
@@ -0,0 +1,11 @@
+export const poseDatabase = {
+    prone: [ /* ...複製原始資料... */ ],
+    front: [ /* ...複製原始資料... */ ],
+    back: [ /* ...複製原始資料... */ ],
+    sitting: [ /* ...複製原始資料... */ ],
+    lying: [ /* ...複製原始資料... */ ],
+    squatting: [ /* ...複製原始資料... */ ],
+    leg_focus: [ /* ...複製原始資料... */ ]
+};
+
+export const handActionList = [ /* ...複製原始資料... */ ];
